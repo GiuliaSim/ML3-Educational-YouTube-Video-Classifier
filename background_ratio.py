ceb492d0f82427683663ce6d09a5aa2e4d086653
Tri Breiting Valle <trine.breiting@gmail.com>
2019-02-24 00:17:13 +0100

    feature Background Lightning Ratio

---
+++ background_ratio.py
@@ -0,0 +1,47 @@
+import numpy as np
+import os
+import math
+import statistics
+import cv2 as cv2
+
+frames_dir = "frames\\{}\\"
+file_name="temp.mp4"
+frameBg_name="temp-fram.jpg"
+
+def get_background_lightning_ratio(videoId):
+    b = []
+    for file in os.listdir(frames_dir.format(videoId)):
+        if file.endswith(".bmp"):
+            image_path = os.path.join(frames_dir.format(videoId), file)
+            print (image_path)
+            #img = cv2.imread(image_path)
+            #img = cv2.cvtColor(image_path, cv2.COLOR_BGR2GRAY)
+            iesimo = subtract_background(image_path, videoId)
+            if (iesimo is not None):
+            	b.append(iesimo)            
+    print ("totale :")
+    return statistics.mean(b)
+
+def subtract_background(image_path, videoId):
+	fgbg = cv2.createBackgroundSubtractorMOG2()
+	frame = cv2.imread(image_path)
+	fgmask = fgbg.apply(frame)
+	cv2.imwrite((frames_dir.format(videoId) + frameBg_name), fgmask)
+	frameBg = cv2.imread(frames_dir.format(videoId) + frameBg_name)
+
+	luminositaBack = calcoloBightness(frameBg)
+	luminositaFrame = calcoloBightness(frame)
+	luminotsitaSogetto = (luminositaFrame - luminositaBack)
+
+	if(luminositaBack != 0):
+		a = math.pow((luminotsitaSogetto / luminositaBack),2)
+		a = math.sqrt(a)
+		b = np.log2(a)
+		print(b)
+	return b
+       	
+def calcoloBightness(image):
+	image_hsv = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+	h, s, v = cv2.split(image_hsv)
+	brightness = v.mean()
+	return brightness
