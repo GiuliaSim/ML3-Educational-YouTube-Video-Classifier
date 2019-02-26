29b906922700ac212f36d612aea1f3e2e211a425
GiuliaSim <giuly.simonelli@gmail.com>
2019-02-26 20:11:10 +0100

    add features mfcc

--- get_features_audio.py
+++ get_features_audio.py
@@ -7,6 +7,7 @@ import time
 import concurrent.futures
 import csv
 from os.path import getsize
+from pprint import pprint
 
 INPUT_FILE = "data\\dataset.csv"
 audios_dir = "audios\\"
@@ -27,34 +28,64 @@ def get_features(row):
 			fs, signal = wav.read(file_name)
 			signal = signal[:,0]
 
-			# Example of pre-emphasizing.
+			# Example of pre-emphasizing:
+			# Preemphasising on the signal.
+			# This is a preprocessing step.
 			signal_preemphasized = speechpy.processing.preemphasis(signal, cof=0.98)
 
-			# Example of staching frames
+			# Example of staching frames: 
+			# Create stacking frames from the raw signal.
 			frames = speechpy.processing.stack_frames(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01, filter=lambda x: np.ones((x,)),
 			         zero_padding=True)
 
-			# Example of extracting power spectrum
+			# Example of FFT Spectrum:
+			# Calculation of the Fast Fourier Transform.
+			fft_spectrum = speechpy.processing.fft_spectrum(frames, fft_points=512)
+			#print('Fast Fourier Transform=', fft_spectrum.shape)
+
+			# Example of extracting power spectrum:
+			# Power Spectrum calculation.
 			power_spectrum = speechpy.processing.power_spectrum(frames, fft_points=512)
-			print('power spectrum shape=', power_spectrum.shape)
+			#print('power spectrum shape=', power_spectrum.shape)
+
+			# Example of extracting log power spectrum:
+			# Log Power Spectrum calculation.
+			log_power_spectrum = speechpy.processing.log_power_spectrum(frames, fft_points=512, normalize=True)
+			#print('log power spectrum shape=', log_power_spectrum.shape)
 
 			############# Extract MFCC features #############
 			mfcc = speechpy.feature.mfcc(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
-			             num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
-			mfcc_cmvn = speechpy.processing.cmvnw(mfcc,win_size=301,variance_normalization=True)
-			print('mfcc(mean + variance normalized) feature shape=', mfcc_cmvn.shape)
+			             num_cepstral=13, num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
+			
+			mfcc_cmvn = speechpy.processing.cmvn(mfcc,variance_normalization=True)
+			#print('mfcc(mean + variance normalized) feature shape=', mfcc_cmvn.shape)
+
+			mfcc_cmvnw = speechpy.processing.cmvnw(mfcc,win_size=301,variance_normalization=True)
+			#print('mfcc(mean + variance normalized) over the sliding window feature shape=', mfcc_cmvnw.shape)
 
 			mfcc_feature_cube = speechpy.feature.extract_derivative_feature(mfcc)
-			print('mfcc feature cube shape=', mfcc_feature_cube.shape)
+			#print('mfcc feature cube shape=', mfcc_feature_cube.shape)
 
 			############# Extract logenergy features #############
 			logenergy = speechpy.feature.lmfe(signal, sampling_frequency=fs, frame_length=0.020, frame_stride=0.01,
 			             num_filters=40, fft_length=512, low_frequency=0, high_frequency=None)
 			logenergy_feature_cube = speechpy.feature.extract_derivative_feature(logenergy)
-			print('logenergy features=', logenergy.shape)
+			#print('logenergy features=', logenergy.shape)
 
 			print(f'Process {os.getpid()} done procesing record {videoId} at {time.strftime("%H:%M")}')
 
+			return {
+				'videoId': videoId,
+				'fft_spectrum': fft_spectrum.shape,
+				'power_spectrum': power_spectrum.shape,
+				'log_power_spectrum': log_power_spectrum.shape,
+				'mfcc': mfcc.shape,
+				'cmvn': mfcc_cmvn.shape,
+				'cmvnw': mfcc_cmvnw.shape,
+				'mfcc_feature_cube_shape': mfcc_feature_cube.shape,
+				'logenergy_features': logenergy.shape
+			}
+		return {'videoId':videoId}
 
 if __name__ == '__main__':
 	start = time.time()
@@ -62,6 +93,8 @@ if __name__ == '__main__':
 		with concurrent.futures.ThreadPoolExecutor() as executor:
 			result = executor.map(get_features, csv.reader(input))
 	end = time.time()
+
+	pprint(tuple(result))
 	print(f'\nTime to complete: {format_time(start, end)}\n')	
 
 # if __name__ == '__main__':
