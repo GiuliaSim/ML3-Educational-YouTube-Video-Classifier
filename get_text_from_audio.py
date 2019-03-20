8e6a6e70ffbdb17180ef7dab190ff1d3ea1b7af2
Pably8D <trine.breiting@gmail.com>
2019-03-20 18:23:15 +0100

    modify script

--- get_text_from_audio.py
+++ get_text_from_audio.py
@@ -2,17 +2,34 @@ import os
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.utils import make_chunks
+import time
+import csv
+import json
 
-path_audios="audios\{}\\"
-name_file_audio = ".\\audios\\{}\\{}_temp.wav"
-name_frame_file_audio = "audios\\{}\\{}_frame\\{}_{}.wav"
-path_frames_audio = "audios\\{}\\{}_frame\\"
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+from nltk.corpus import stopwords
+import matplotlib.pyplot as plt
+import operator
+
+OUTPUT_FILE = "data\\dataset_text_analysis.csv"
+INPUT_FILE = "data\\dataset.csv"
+path_audios="D:\\ProgettoMLSII\\audios\{}\\"
+name_file_audio = "D:\\ProgettoMLSII\\audios\\{}\\{}_temp.wav"
+name_frame_file_audio = "D:\\ProgettoMLSII\\audios\\{}\\{}_frame\\{}_{}.wav"
+path_frames_audio = "D:\\ProgettoMLSII\\audios\\{}\\{}_frame\\"
 
 def get_text(videoId):
 	path_audio = name_file_audio.format(videoId,videoId)
-	file = create_folder_file(videoId)
-	create_frame_audio(path_audio, videoId)
-	text(videoId, file)
+	if  os.path.exists(path_audio):
+		if not os.path.exists((path_frames_audio.format(videoId, videoId)+"text.txt")):
+			file = create_folder_file(videoId)
+			create_frame_audio(path_audio, videoId)
+			text(videoId, file)
+		return get_analisi_testo((path_frames_audio.format(videoId, videoId)+"text.txt"))
+
+
 
 def create_folder_file(videoId):
 	if not os.path.exists(path_frames_audio.format(videoId, videoId)):
@@ -33,7 +50,7 @@ def create_frame_audio(path_audio, videoId):
 
 def text (videoId, file):
 	i = 0
-	for frame in os.listdir(path_frames_audio.format(videoId, videoId)):
+	for frame in os.listdir(path_frames_audio.format(videoId, videoId)): 
 		if frame.endswith(".wav"):
 			r=sr.Recognizer()
 			path_frame=name_frame_file_audio.format(videoId, videoId, videoId, i)
@@ -47,3 +64,51 @@ def text (videoId, file):
 			except Exception as e:
 				print(e)
 	file.close() 
+
+def get_analisi_testo(path_text):
+#if __name__ == '__main__':
+	f = open(path_text, "r") 
+	input=f.read()
+	tokenized_word=word_tokenize(input)
+	stop_words=set(stopwords.words("english"))
+	ciccia=['I', '\'s', 'one', 'and','I','A','And','So','arnt','This', 'way' 'also','When','It','many','Many','so','cant','Yes','yes','No','no','These','these', ',']
+	stop_words.update(ciccia)
+	tokenized_sent = tokenized_word
+	filtered_sent=[]
+	for w in tokenized_sent:
+		if w not in stop_words:
+			filtered_sent.append(w)
+	fdist = FreqDist(filtered_sent)
+	mappa = sorted(fdist.items(), key=operator.itemgetter(1), reverse=True)
+
+	s= '{'
+	#print(json.dumps(mappa, ensure_ascii=False))
+	for key,val in mappa:
+
+		vall= str(val)+','
+		s+=key+':'+vall
+	if len(s) > 1 :
+		s = s[:-1]
+	s = s + '}'
+	#print(s)
+	return s
+
+
+if __name__ == '__main__':
+	start = time.time()
+	with open(INPUT_FILE, encoding="utf8") as input , open(OUTPUT_FILE, 'w', newline='') as output:
+		writer = csv.writer(output, delimiter=",")
+		header = ['videoId','work frequency']
+		writer.writerow(header)
+		for row in csv.reader(input):
+			if row[0] != 'videoId':
+				print(f'Inizio testo at {time.strftime("%H:%M")}')
+				results = []
+				videoId = row[0].replace('/watch?v=','')
+				result = get_text(videoId)
+				element = [videoId, result]
+				writer.writerow(element)
+				print('scritto videoId = ', videoId)				
+
+	end = time.time()
+	print(f'\nTime to complete: {format_time(start, end)}\n')
