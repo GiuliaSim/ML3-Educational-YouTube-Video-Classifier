7a638f359216e2313ea6c9eed511e609ce495071
GiuliaSim <giuly.simonelli@gmail.com>
2019-03-23 19:02:18 +0100

    convert data type

---
+++ data/convert_type.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+DATA_FILE = 'all_dataset.csv'
+OUTPUT_FILE = 'all_dataset1.csv'
+
+a = pd.read_csv(DATA_FILE)
+a['IsEducational'] = a['IsEducational'].astype('bool')
+
+a.to_csv(OUTPUT_FILE, index=False)
