da77e8d30afee55c7c1d1a4a29ad291e58fb0755
GiuliaSim <giuly.simonelli@gmail.com>
2019-03-29 12:29:19 +0100

    move data

--- data/convert_type.py
+++ data/convert_type.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 DATA_FILE = 'all_dataset.csv'
-OUTPUT_FILE = 'all_dataset1.csv'
+OUTPUT_FILE = 'all_dataset.csv'
 
 a = pd.read_csv(DATA_FILE)
 a['IsEducational'] = a['IsEducational'].astype('bool')
